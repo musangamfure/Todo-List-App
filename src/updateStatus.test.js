769e9fda35976780d774d6c1a8dd68f0de5a7a18
Musangamfura Emmanuel <musangamfure@gmail.com>
2023-05-12 14:44:00 +0100

    Update Test files

--- src/updateStatus.test.js
+++ src/updateStatus.test.js
@@ -3,8 +3,8 @@ const todo = {
   description: String,
   completed: false,
 };
-const inputField = document.createElement('input');
-inputField.value = 'one';
+const inputField = document.createElement("input");
+inputField.value = "one";
 
 const addTodo = () => {
   if (inputField.value.length !== 0) {
@@ -13,7 +13,7 @@ const addTodo = () => {
     for (let i = 0; i < todoArray.length; i += 1) {
       todoArray[i].index = i + 1;
     }
-    localStorage.setItem('todos', JSON.stringify(todoArray));
+    localStorage.setItem("todos", JSON.stringify(todoArray));
   }
 };
 
@@ -23,41 +23,41 @@ const editTodo = (id, newInput) => {
     item.description = newInput;
   }
 
-  localStorage.setItem('todos', JSON.stringify(todoArray));
+  localStorage.setItem("todos", JSON.stringify(todoArray));
 };
 
 const updateTodoStatus = (id) => {
   const checked = todoArray.find((item) => item.index === id);
   if (checked.completed === false) {
     checked.completed = true;
-    localStorage.setItem('todos', JSON.stringify(todoArray));
+    localStorage.setItem("todos", JSON.stringify(todoArray));
   } else if (checked.completed === true) {
     checked.completed = false;
-    localStorage.setItem('todos', JSON.stringify(todoArray));
+    localStorage.setItem("todos", JSON.stringify(todoArray));
   }
 };
 
 const clearAll = () => {
   const checkedTodos = todoArray.filter((item) => item.completed === false);
   todoArray = checkedTodos;
-  localStorage.setItem('todos', JSON.stringify(todoArray));
+  localStorage.setItem("todos", JSON.stringify(todoArray));
   return todoArray;
 };
 
-describe('todo', () => {
-  test('add a todo to todo array', () => {
+describe("todo", () => {
+  test("add a todo to todo array", () => {
     addTodo();
     expect(todoArray.length).toBe(1);
   });
-  test('edit todo description with value of two', () => {
-    editTodo(1, 'two');
-    expect(todoArray[0].description).toBe('two');
+  test("should check edit todo description with value of two", () => {
+    editTodo(1, "two");
+    expect(todoArray[0].description).toBe("two");
   });
-  test('check if completed status is updated', () => {
+  test("should check if completed status is updtaded", () => {
     updateTodoStatus(1);
     expect(todoArray[0].completed).toBe(true);
   });
-  test('test clear all completed', () => {
+  test("Should clear all completed todos", () => {
     updateTodoStatus(1);
     clearAll();
     for (let i = 0; i < todoArray.length; i += 1) {
